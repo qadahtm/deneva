59d360d5d77c23f15d05f05307209c9bd395f092
Thamir Qadah <qadah.thamir@gmail.com>
2020-05-04 19:12:08 -0400

    updated init_hosts

--- ansible/init_hosts.py
+++ ansible/init_hosts.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 import sys, getopt
-
 import yaml
 
 
 def usage():
     print('init_hosts.py [-h, --ceploy-home=/path/to/ceploy, --conf-file=/path/to/ceploy-conf] <inventory directory>')
 
+def delete_hosts(opts, args):
+    pass
 
 def get_vm_name(prefix, n):
     return "{}-{}-{}".format(prefix, n['group'], n['name'])
@@ -15,7 +16,7 @@ def get_vm_name(prefix, n):
 def main(argv):
 
     try:
-        opts, args = getopt.getopt(argv,"h",["help", "ceploy-home=", "conf-file="])
+        opts, args = getopt.getopt(argv,"hd",["help", "delete-hosts", "ceploy-home=", "conf-file="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -24,18 +25,20 @@ def main(argv):
     ceploy_home = environ.get("CEPLOY_HOME")
     cloud_conf_file = environ.get("CEPLOY_CONF_FILE")
 
+    if len(args) != 1:
+        print("Need to specify target inventory directory")
+        usage()
+        sys.exit(1)
+
     for o, a in opts:
         if o == '--conf-file':
             cloud_conf_file = a
         elif o == '--ceploy-home':
             ceploy_home = a
+        elif o == '--delete-hosts':
+            delete_hosts(opts, args)
         elif o == '-h':
             usage()
-
-    if len(args) != 1:
-        print("Need to specify target inventory directory")
-        usage()
-        sys.exit(1)
 
     sys.path.append("{}/src".format(ceploy_home))
 
@@ -49,6 +52,13 @@ def main(argv):
     # initiate Cloud context
     cloud = Cloud.make(Provider.GCLOUD, cloud_conf_file)
 
+    deploy_vms_and_create_hosts_file(args, cloud)
+
+
+def deploy_vms_and_create_hosts_file(args, cloud):
+
+    from ceploy.providers.gcloud import GCVM
+
     with open('../site/site.yml') as site_file:
         site = yaml.full_load(site_file)
         from pprint import pprint
@@ -61,35 +71,48 @@ def main(argv):
         default_group = {}
 
         for n in site['nodes']:
-            pprint(n)
-            # cloud.create_instance()
-
             group = n['group']
             vm_name = get_vm_name(prefix, n)
-            vm = cloud.create_instance(vm_name, n['template'], n['zone'])
-            final_vm = {}
-            final_vm['vm'] = vm
-            final_vm['conf'] = n
+            status, vm, err = cloud.create_instance(vm_name, n['template'], n['zone'])
+            if status:
+                final_vm = {}
+                final_vm['vm'] = GCVM(vm[0])
+                final_vm['vm_raw'] = vm[0]
+                final_vm['conf'] = n
 
-            if group == 'clients':
-                clients[vm_name] = final_vm
-            elif group == 'zookeeper':
-                zookeepers[vm_name] = final_vm
-            elif group == 'servers':
-                servers[vm_name] = final_vm
+                if group == 'clients':
+                    clients[vm_name] = final_vm
+                elif group == 'zookeeper':
+                    zookeepers[vm_name] = final_vm
+                elif group == 'servers':
+                    servers[vm_name] = final_vm
+                else:
+                    # default is server group
+                    vm_name = "{}-{}".format(prefix, n['name'])
+                    default_group[vm_name] = final_vm
             else:
-                # default is server group
-                vm_name = "{}-{}".format(prefix, n['name'])
-                default_group[vm_name] = final_vm
+                print("Error in create instance.")
+                pprint(err)
 
+        with open('./{}/hosts'.format(args[0]), 'w') as hosts_file:
 
-        hostfile = ""
-        for d in default_group:
-            print(default_group[d])
+            for n in default_group:
+                print(default_group[n]['vm'].ext_ip, file=hosts_file)
 
-        pprint(servers)
+            if len(servers) > 0:
+                print("[servers]", file=hosts_file)
+            for n in servers:
+                print(servers[n]['vm'].ext_ip, file=hosts_file)
 
+            if len(clients) > 0:
+                print("[clients]", file=hosts_file)
+            for n in clients:
+                print(clients[n]['vm'].ext_ip, file=hosts_file)
 
+            if len(zookeepers) > 0:
+                print("[zookeeper]", file=hosts_file)
+            for n in zookeepers:
+                print(zookeepers[n]['vm'].ext_ip, file=hosts_file)
 
 
 if __name__ == "__main__":
